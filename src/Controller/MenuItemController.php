e789fdafc5eb0199c2359b500b7625fc80223af1
WoodenToaster <monizvalentine@yahoo.fr>
2018-09-27 15:21:27 +0200

    add PDO trait + test bdd

--- src/Controller/MenuItemController.php
+++ src/Controller/MenuItemController.php
@@ -5,17 +5,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+require('PDO.php');
 
 class MenuItemController extends AbstractController
 {
     /**
      * @Route("/menu")
      */
+
+    use PDO;
     public function menu()
     {
-         return $this->render('menu.html.twig');
-    }
+        $pdo = PDO::getInstance();
+        $req = $pdo->prepare("SELECT * FROM doupoils.animal");
+        
+        $req->execute();
+        $array = $req->fetch($pdo::FETCH_ASSOC);
+		return $this->render('menu.html.twig', ['animals' => $array,]); 
+	}
 }
-
-
 ?>
