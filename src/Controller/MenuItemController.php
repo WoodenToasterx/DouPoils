04dcf92a2185c40d258251e70607a203bb971ded
WoodenToaster <monizvalentine@yahoo.fr>
2018-09-30 14:45:26 +0200

    Dev 2018-09-30
    ajout footer, header, accueil
    lien css
    ajout image

--- src/Controller/MenuItemController.php
+++ src/Controller/MenuItemController.php
@@ -5,23 +5,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-require('PDO.php');
-
 class MenuItemController extends AbstractController
 {
     /**
      * @Route("/menu")
      */
 
-    use PDO;
     public function menu()
     {
-        $pdo = PDO::getInstance();
-        $req = $pdo->prepare("SELECT * FROM doupoils.animal");
-        
-        $req->execute();
-        $array = $req->fetch($pdo::FETCH_ASSOC);
-		return $this->render('menu.html.twig', ['animals' => $array,]); 
+        return $this->render('accueil.html.twig'); 
 	}
 }
 ?>
