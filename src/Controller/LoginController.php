6b30bc3afc8e996d838c354ddeb5b980c0fef2e4
WoodenToaster <monizvalentine@yahoo.fr>
2018-10-08 11:23:15 +0200

    add login + link from header

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login")
+     */
+
+    public function login()
+    {
+        return $this->render('login.html.twig');
+	}
+}
+?>
