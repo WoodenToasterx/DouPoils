6d057ae1b557a056c2b151acb3f09aa8aee2579d
WoodenToaster <monizvalentine@yahoo.fr>
2019-01-04 14:37:22 +0100

    contactform

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,6 +6,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactController extends AbstractController
 {
@@ -13,11 +18,38 @@ class ContactController extends AbstractController
      * @Route("/contact")
      */
 
-    public function contact()
+    public function contact(Request $request)
     {
-        return $this->render('contact.html.twig');
+        $session=$this->get('session');
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $defaultData = array('message' => 'Type your message');
+
+        $form = $this->createFormBuilder($defaultData)
+            ->add('Name', TextType::class)
+            ->add('Email', TextType::class)
+            ->add('Message', TextareaType::class)
+            ->add('Save', SubmitType::class, array('label' => 'Valider', 'attr' =>  array('class' => 'btn btn-primary' )))
+            ->getForm();
+        
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $contacter = $form->getData();
+
+            try
+            {
+                
+            }
+            catch(Exception $e)
+            {
+                
+            }
+        }
+
+        return $this->render('contact.html.twig', array('form' => $form->createView(),));
     }
-    
-    
 }
 ?>
